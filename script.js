7e29c8d798be57446736cda17283cc836aebc5ac
Hans Reichgelt <hreichgelt06@gmail.com>
2022-10-14 16:41:30 -0500

    working in script file

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hotmoms')
+let numberOfSquares = 6;
+let colors = [];
+let pickedColor;
+let squares = document.querySelectorAll(".square");
+let colorDisplay = document.querySelector("displayColor");
+let messageDisplay = document.querySelector("#message");
+let heading = document.querySelector("#heading");
+let resetButton = document.querySelector("#reset");
+let modeButtons = document.querySelectorAll(".mode");
+
+init();
+
