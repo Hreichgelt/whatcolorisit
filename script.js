42c61bfe1e1969f75dade2f840158202531a46ab
Hans Reichgelt <hreichgelt06@gmail.com>
2022-10-15 08:54:28 -0500

    Easy and hard modes square colors and picked colors

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ let modeButtons = document.querySelectorAll(".mode");
 
 init();
 
+function init() {
+    setupModeBtns();
+    setupSquares();
+    reset();
+}
+
+function setupModeBtns() {
+    for (let i = 0; i < modeButtons.length; i ++) {
+        modeButtons[i].addEventListener("click", function() {
+            modeButtons[0].classList.remove("selected");
+            modeButtons[1].classList.remove("selected");
+            this.classList.add("selected");
+            this.textContent === "Easy" ? (numberOfSquares = 3) : (numberOfSquares = 6);
+            reset();
+        });
+    }
+}
+
+function setupSquares() {
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].addEventListener("click", function() {
+            let clickedColor = this.style.backgroundColor;
+
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = 'Correct';
+                changeColors(clickedColor);
+                heading.style.backgroundColor = clickedColor;
+                resetButton.textContent = "Play Again?";
+            } else {
+                this.style.backgroundColor = "#232323";
+                messageDisplay.textContent = "Try again";
+            }
+        });
+    }
+}
